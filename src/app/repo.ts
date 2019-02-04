2e00fd2747a28efb83819f4478df5cdfb7af6c1c
wecode <umurangamclaire@gmail.com>
2019-02-04 14:54:11 +0200

    add all changes

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,5 @@
+// export class Repo {
+// }
 export class Repo {
-}
+    constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string ) {}
+    }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,7 @@
+// export class User {
+// }
 export class User {
+    constructor( public name: string, public login: string, public avatar_url: string, public email: string, public location: string, public public_repos:number , public html_url:string) {
+    }
+  
 }
